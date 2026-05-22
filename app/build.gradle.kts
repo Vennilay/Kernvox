d8f9762883ae87dfeb5ca13dac022463ff2da3ca
Makar <makarleonardov@gmail.com>
2026-05-22 12:36:02 +0300

    chore: add kotlinx-coroutines-test dependency for future ViewModel tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
 
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
